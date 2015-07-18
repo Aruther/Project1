8934c9ecbcd85f7dae48daeb8f3cb2df9960d971
Dean Brown <deanbrown@Deans-iMac.local>
2015-07-17 20:32:21 -0700

    Initial Commit

---
+++ Conversion Mock 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conversion Mock 3
+//
+//  Created by Dean Brown on 7/17/15.
+//  Copyright (c) 2015 Parker Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
